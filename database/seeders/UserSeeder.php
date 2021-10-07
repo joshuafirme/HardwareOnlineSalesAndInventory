a73021418175018f437e4136e4fe50efcf68c2f4
josh <61042416+joshuafirme@users.noreply.github.com>
2021-10-07 15:38:11 +0800

    Update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,14 +13,34 @@ class UserSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   
+
         \DB::table('users')->insert([
-            'name' => 'Kenneth',
-            'username' => 'kenneth',
-            'email' => 'test@gmail.com',
-            'password' => \Hash::make('12345'),
-            'access_level' => 4,
-            'status' => 1
+            [
+                'name' => 'Aina Marie Gayle Gonzales',
+                'username' => 'ainamarie',
+                'email' => 'test21@gmail.com',
+                'password' => \Hash::make('12345'),
+                'access_level' => 4,
+                'status' => 1
+            ],
+            [
+                'name' => 'Honey Grace Causapin',
+                'username' => 'honeygrace',
+                'email' => 'test3@gmail.com',
+                'password' => \Hash::make('12345'),
+                'access_level' => 4,
+                'status' => 1
+            ],
+            [
+                'name' => 'Catherine Medina',
+                'username' => 'catherine',
+                'email' => 'test4@gmail.com',
+                'password' => \Hash::make('12345'),
+                'access_level' => 4,
+                'status' => 1
+            ]   
         ]);
+       
     }
 }
