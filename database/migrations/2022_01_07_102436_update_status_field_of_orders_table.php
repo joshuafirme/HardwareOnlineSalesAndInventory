5442e5fae2b19eac5986543523a70b0f43881900
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-07 10:25:27 +0800

    Create 2022_01_07_102436_update_status_field_of_orders_table.php

---
+++ database/migrations/2022_01_07_102436_update_status_field_of_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStatusFieldOfOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('status')->default(1)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
