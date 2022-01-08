d4eae50fb4127e0c2994f32c09a006537becf4d5
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-08 15:50:34 +0800

    Update index.blade.php

--- resources/views/admin/utilities/archive/index.blade.php
+++ resources/views/admin/utilities/archive/index.blade.php
@@ -27,16 +27,6 @@
     
 
         <div class="row">
-            <div class="col-12">
-                <div class="float-left mt-2">
-                    Date
-                </div>
-                <input type="date" class="form-control w-auto float-left m-1" name="date_from" id="date_from" value="{{ date('Y-m-d') }}">
-                <div class="float-left mt-2">
-                    -
-                </div>
-                <input data-column="9" type="date" class="form-control w-auto float-left m-1" name="date_to" id="date_to" value="{{ date('Y-m-d') }}">  
-            </div>
           <div class="col-md-12 col-lg-12 mt-3">
             <div class="card">
                 <div class="card-body">
